7f5f6cc29b65531ff2bad96522b0c8f28737e9da
Olawuwo Abideen <76418051+olawuwo-abideen@users.noreply.github.com>
2023-09-12 21:13:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const { DateTime } = require('luxon');
 
-const GITHUB_REPO_URL = 'https://github.com/olawuwo-abideen/HNG';
+const GITHUB_REPO_URL = 'https://github.com/olawuwo-abideen/hngtaskone';
 
 app.get('/api', (req, res) => {
   const slackName = req.query.slack_name || 'olawuwo abideen';
@@ -14,7 +14,7 @@ app.get('/api', (req, res) => {
     current_day: now.toFormat('cccc'),
     utc_time: now.toUTC().toISO(),
     track: track,
-    github_file_url: 'https://github.com/olawuwo-abideen/HNG/blob/main/task_one/app.js',
+    github_file_url: 'https://github.com/olawuwo-abideen/hngtaskone/blob/main/app.js',
     github_repo_url: GITHUB_REPO_URL,
     status_code: 200,
   };
